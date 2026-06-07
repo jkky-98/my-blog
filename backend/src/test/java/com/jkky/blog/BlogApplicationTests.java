3afe2c962aed26c2dea8a777baa79bb806b8e153
jkky-98 <aal2525@ajou.ac.kr>
2026-06-07 14:50:27 +0900

    chore: "스프링 부트 백엔드 프로젝트를 추가"

---
+++ backend/src/test/java/com/jkky/blog/BlogApplicationTests.java
@@ -0,0 +1,14 @@
+package com.jkky.blog;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class BlogApplicationTests {
+
+	@Test
+	void applicationClassExists() {
+		assertNotNull(BlogApplication.class);
+	}
+
+}
